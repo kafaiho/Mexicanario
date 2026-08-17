ba33da28c722be3ea5644a6871d8881e128ee207
kafaiho <kafaihoz@gmail.com>
2026-08-17 16:37:28 -0500

    test: add cultural content test runner

---
+++ scripts/run-tests.js
@@ -0,0 +1,17 @@
+const { spawnSync } = require('node:child_process');
+
+const testModules = [
+  'src/utils/textUtils.test.js',
+  'src/config/culturalTaxonomy.test.js',
+  'src/config/culturalContent.test.js',
+];
+
+for (const testModule of testModules) {
+  const result = spawnSync(process.execPath, [testModule], {
+    stdio: 'inherit',
+  });
+
+  if (result.status !== 0) {
+    process.exit(result.status ?? 1);
+  }
+}
